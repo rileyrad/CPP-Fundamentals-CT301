9afa31bfcd41968ed7a85313e1379bcf63aefcf2
Riley Radici <99680138+rileyrad@users.noreply.github.com>
2024-09-13 13:21:01 -0600

    Initial upload

---
+++ hw0/hw0.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+// Knock Knock
+cout << "Knock Knock\nWho's There?\n"
+<< "Dejav\n" << "Dejav who?\n" << "Knock Knock\n";
+    return 0;
+}
---
+++ hw1/CMakeFiles/HW1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1.dir/mad_libs.cc.o"
+  "CMakeFiles/HW1.dir/mad_libs.cc.o.d"
+  "HW1"
+  "HW1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
